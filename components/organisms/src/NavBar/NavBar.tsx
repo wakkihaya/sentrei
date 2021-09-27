27444cd30076a8f09d6a2a35f76182f7fabea64c
wakkihaya <wakkihaya@gmail.com>
2021-09-27 09:27:39 +0900

    Revert "feat: update modal for item"
    This reverts commit baa5486726345a06587cfba5a5a0d92732a5f2c2.

--- components/organisms/src/NavBar/NavBar.tsx
+++ components/organisms/src/NavBar/NavBar.tsx
@@ -1,10 +1,9 @@
 import { Link, Image } from "@sentrei/atoms";
 import { useModalScreen } from "@sentrei/hooks";
-import { ModalScreen } from "@sentrei/molecules";
 
 import clsx from "clsx";
+
 import type { FC, ReactNode } from "react";
-import { createPortal } from "react-dom";
 
 interface navItem {
   url?: string;
@@ -17,47 +16,28 @@ export interface NavBarProps {
   className?: string;
   logoSrc: string;
   items: navItem[];
-  bgColor?: string;
 }
 
 export const NavBar: FC<NavBarProps> = props => {
-  const { className, logoSrc, items, bgColor = "bg-black" } = props;
+  const { className, logoSrc, items } = props;
   const [, setModalOpen] = useModalScreen();
 
   const openModal = () => {
     setModalOpen(true);
   };
-
-  const Modal: FC<{ children: ReactNode }> = ({ children }) => {
-    return createPortal(
-      <ModalScreen>
-        <div
-          className={clsx(
-            "inline-block overflow-y-scroll p-6 my-8 w-full md:w-[80%] h-[90vh] md:h-[80vh] text-left align-middle bg-radial rounded-2xl shadow-2xl opacity-95 drop-shadow-2xl transition-all transform",
-            bgColor,
-          )}
-        >
-          {children}
-        </div>
-      </ModalScreen>,
-      document.getElementById("root"),
-    );
-  };
-
   const renderItems = () => {
     return items.map(item => {
       if (item.showModal) {
         return (
           <>
-            {/* eslint-disable-next-line jsx-a11y/no-static-element-interactions */}
-            <div
+            {/* <div
               className="text-white visited:text-white hover:text-white no-underline hover:underline cursor-pointer"
-              onKeyDown={openModal}
               onClick={openModal}
             >
               {item.title}
-            </div>
-            <Modal>{item.modalContent}</Modal>
+            </div> */}
+            {/* <ModalScreen>{item.modalContent}</ModalScreen> //TODO: show
+            modal */}
           </>
         );
       } else {
@@ -76,7 +56,7 @@ export const NavBar: FC<NavBarProps> = props => {
 
   return (
     <>
-      <header className={clsx(bgColor, className)}>
+      <header className={clsx("bg-indigo-600", className)}>
         <nav
           className="px-4 sm:px-6 lg:px-8 mx-auto max-w-7xl"
           aria-label="Top"
